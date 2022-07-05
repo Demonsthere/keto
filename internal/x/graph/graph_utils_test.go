f47fe4df01d68102053387f884c312fa8143e168
Patrik <zepatrik@users.noreply.github.com>
2022-07-05 13:21:05 +0200

    fix: race condition in graph context helper

--- internal/x/graph/graph_utils_test.go
+++ internal/x/graph/graph_utils_test.go
@@ -2,6 +2,7 @@ package graph
 
 import (
 	"context"
+	"sync"
 	"testing"
 
 	"github.com/ory/keto/internal/relationtuple"
@@ -11,7 +12,6 @@ import (
 
 func TestEngineUtilsProvider_CheckVisited(t *testing.T) {
 	t.Run("case=finds cycle", func(t *testing.T) {
-
 		linkedList := []relationtuple.SubjectSet{{
 			Namespace: "default",
 			Object:    "A",
@@ -47,7 +47,6 @@ func TestEngineUtilsProvider_CheckVisited(t *testing.T) {
 	})
 
 	t.Run("case=ignores if no cycle", func(t *testing.T) {
-
 		list := []relationtuple.SubjectSet{{
 			Namespace: "default",
 			Object:    "A",
@@ -80,5 +79,38 @@ func TestEngineUtilsProvider_CheckVisited(t *testing.T) {
 		}
 
 		assert.Equal(t, isThereACycle, false)
+	})
+
+	t.Run("case=no race condition during adding", func(t *testing.T) {
+		// we repeat this test a few times to ensure we don't have a race condition
+		// the race detector alone was not able to catch it
+		for i := 0; i < 500; i++ {
+			subject := &relationtuple.SubjectSet{
+				Namespace: "default",
+				Object:    "racy",
+				Relation:  "connected",
+			}
+
+			ctx, _ := CheckAndAddVisited(context.Background(), &relationtuple.SubjectSet{Object: "just to setup the context"})
+			var wg sync.WaitGroup
+			var aCycle, bCycle bool
+			var aCtx, bCtx context.Context
+
+			wg.Add(2)
+			go func() {
+				aCtx, aCycle = CheckAndAddVisited(ctx, subject)
+				wg.Done()
+			}()
+			go func() {
+				bCtx, bCycle = CheckAndAddVisited(ctx, subject)
+				wg.Done()
+			}()
+
+			wg.Wait()
+			// one should be true, and one false
+			assert.False(t, aCycle && bCycle)
+			assert.True(t, aCycle || bCycle)
+			assert.Equal(t, aCtx, bCtx)
+		}
 	})
 }
